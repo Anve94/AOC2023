68f95eaac58f71a8c0d9db86f26449e85f62f52a
Stefan Boonstra <boonstra.stefan.wp@gmail.com>
2023-11-30 21:43:21 +0100

    Initial simple file parser
    * Add file parser to allow parsing of file where every line is considered a value
    * Add basic unit tests for file parser

---
+++ lib/Support/Model/InputParser.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Support\Model;
+
+use InvalidArgumentException;
+use Support\Api\InputParserInterface;
+
+class InputParser implements InputParserInterface
+{
+    private const EOL_REGEX = "/\\r\\n|\\r|\\n/";
+
+    /**
+     * @inheritDoc
+     * @throws InvalidArgumentException
+     */
+    public static function parseFileAsArraySplitOnLines(string $filePath): array
+    {
+        static::validateFileExists($filePath);
+
+        $data = file_get_contents($filePath);
+        return preg_split(self::EOL_REGEX, $data);
+    }
+
+    private static function validateFileExists(string $filePath): void
+    {
+        if (!file_exists($filePath)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'File does not exist. Did you pass the correct filePath? Received: %s ',
+                    $filePath
+                )
+            );
+        }
+    }
+
+}
---
+++ tests/InputParserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test;
+
+use PHPUnit\Framework\TestCase;
+use Support\Model\InputParser;
+
+class InputParserTest extends TestCase
+{
+    public function testFileWithOneValuePerLineCanBeLoadedAsArray()
+    {
+        $expectedValue = ['1', '2', '3', '4', '5', 'a', 'b', 'c'];
+        $filePath = __DIR__ . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . 'single-values-on-every-line.txt';
+        $actualValue = InputParser::parseFileAsArraySplitOnLines($filePath);
+        $this->assertSame($expectedValue, $actualValue);
+    }
+
+    public function testExceptionIsThrownForInvalidFilePath()
+    {
+        $filePath = 'some-non-existent-file.txt';
+        $this->expectException('InvalidArgumentException');
+        InputParser::parseFileAsArraySplitOnLines($filePath);
+    }
+
+}
