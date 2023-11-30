c40691d089587200b09f89b77aae47dfc7da5af6
Stefan Boonstra <boonstra.stefan.wp@gmail.com>
2023-11-30 20:55:16 +0100

    Initial project setup

---
+++ tests/BaseSetupTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseSetupTest extends TestCase
+{
+    public function testBaseTestingSuiteCanRun()
+    {
+        $this->assertTrue(true);
+    }
+}
