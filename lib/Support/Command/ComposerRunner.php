bc7a41541bf38f7a5a742112d9e30e033fc9f1a0
Stefan Boonstra <boonstra.stefan.wp@gmail.com>
2023-12-05 20:31:03 +0100

    Update execution time to use hrtime

--- lib/Support/Command/ComposerRunner.php
+++ lib/Support/Command/ComposerRunner.php
@@ -21,9 +21,10 @@ class ComposerRunner
         $solutionClass = "\\App\\Day$day\\Solution";
         $solution = new $solutionClass();
         $inputFilePath = __DIR__ . "/../../../src/Day$day/input.txt";
-        $startTime = microtime(true);
+        $startTime = hrtime(true);
         echo($solution->solve($part, $inputFilePath) . PHP_EOL);
-        $execTime = microtime(true) - $startTime;
+        $endTime = hrtime(true);
+        $execTime = ($endTime - $startTime) / 1e9;
         echo(sprintf("Solution found in %s seconds (%s ms)" . PHP_EOL, $execTime, $execTime * 1000));
     }
 
